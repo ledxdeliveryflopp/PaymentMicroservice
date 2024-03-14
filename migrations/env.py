d851fd2efff0253f0e71ea861f92877d8f49d656
LedxDeliveryFlopp <korstim18@gmail.com>
2024-03-14 18:55:17 +0700

    Add models and configure env.py for alembic.

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,8 @@ from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+from src.settings.db import Base
+
 config = context.config
 
 # Interpret the config file for Python logging.
@@ -16,11 +18,13 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+from src.payment.models import PaymentModel, UserModel
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
